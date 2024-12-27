23590a44db731ae5bf84750c22bb5625c8ad579e
harit <harit.carvia@gmail.com>
2024-12-27 16:20:32 +0530

    updated eslit config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,37 @@ import ContentTable from "@/components/ContentTable";
 import GroupedTreeList from "@/components/GroupedTreeList";
 
 
-const treeData = [
-  {
-    label: "Assets",
-    children: [
-      {
-        label: "CSS",
-        children: [
-          {
-            label: "Main",
-            children: [
-              { label: "docs.css", isFile: true },
-              { label: "README.txt", isFile: true },
-            ],
-          },
-          {
-            label: "Tailwind",
-            children: [
-              { label: "input.css", isFile: true },
-            ],
-          },
-        ],
-      },
-      {
-        label: "JS",
-        children: [
-          { label: "app.js", isFile: true },
-        ],
-      },
-    ],
-  },
-];
+// const treeData = [
+//   {
+//     label: "Assets",
+//     children: [
+//       {
+//         label: "CSS",
+//         children: [
+//           {
+//             label: "Main",
+//             children: [
+//               { label: "docs.css", isFile: true },
+//               { label: "README.txt", isFile: true },
+//             ],
+//           },
+//           {
+//             label: "Tailwind",
+//             children: [
+//               { label: "input.css", isFile: true },
+//             ],
+//           },
+//         ],
+//       },
+//       {
+//         label: "JS",
+//         children: [
+//           { label: "app.js", isFile: true },
+//         ],
+//       },
+//     ],
+//   },
+// ];
 
 export default function Home() {
   return (
